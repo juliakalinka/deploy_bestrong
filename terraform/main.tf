486431f95b2f94fe6b3f0ec18ff7a0d63135987a
Oleh Kuchma <oleg.kuchma11@gmail.com>
2025-04-26 23:34:55 +0300

    terrafrom update2

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,7 @@ resource "azurerm_linux_web_app" "dot-net-app" {
   name                = "dot-net-app"
   location            = azurerm_resource_group.main_rsg.location
   resource_group_name = azurerm_resource_group.main_rsg.name
-  service_plan_id     = azurerm_service_plan.first-plan.id
+  service_plan_id     = azurerm_app_service_plan.first-plan.id
 
   site_config {
     always_on        = true
