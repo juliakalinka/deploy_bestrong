46e2b248e9b7780e4ed864b480c6af858ff264b3
Oleh Kuchma <oleg.kuchma11@gmail.com>
2025-04-27 20:15:08 +0300

    terrafromv9

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.26.0"  # Виберіть потрібну версію
+      version = "4.26.0" 
     }
   }
 }
@@ -10,6 +10,7 @@ terraform {
 
 provider "azurerm" {
   features {}
+  resource_provider_registrations = "none"
   subscription_id = var.subscription_id
   tenant_id = var.tenant_id
   client_id = var.client_id
